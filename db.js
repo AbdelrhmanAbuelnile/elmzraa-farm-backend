2429f90c8c14fc6091538777a21e55f0728de0b5
unknown <abdohani362@gmail.com>
2024-02-14 02:01:13 +0200

    vercel

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/mazra')
-.then(()=> console.log("bd connected successfuly"))
+const dbUrl = process.env.MONGODB_CONNECT_URI;
+mongoose.connect(dbUrl)
+.then(()=> console.log("DB connected successfuly"))
 .catch(err => console.log('err', err))
