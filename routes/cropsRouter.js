aa24329ae1199386dde10f2341aad0e04e26c959
unknown <abdohani362@gmail.com>
2024-02-15 22:45:36 +0200

    Add crop creation and search functionality

--- routes/cropsRouter.js
+++ routes/cropsRouter.js
@@ -1,30 +1,73 @@
 const express = require("express");
 const Crop = require("../models/crops")
+const Farm = require("../models/farm")
 
 const router = express.Router();
 
 // create a crop
 router.post('/', async (req,res)=> {
-  const { Crop_Type, Crop_PlantDate, Crop_HarvestDate} = req.body;
-  const crop = await Crop.create({ Crop_Type, Crop_PlantDate, Crop_HarvestDate})
-  res.send(crop) 
-})
+  const { name, plantDate, harvestDate,count, farmId } = req.body;
+
+  if (!farmId) {
+    return res.status(400).send('Farm ID is required');
+  }
+
+  const farm = await Farm.findById(farmId);
+  if (!farm) {
+    return res.status(404).send('Farm not found');
+  }
+
+  const crop = await Crop.create({ name, plantDate, harvestDate, count, farm: farm._id});
+
+  farm.crops.push(crop._id);
+  await farm.save();
+
+  res.send(crop);
+});
 
 // get all crops
 router.get('/', async (req,res)=> {
-  const crops = await Crop.find();
-  res.send(crops)
-})
+  const {farmId} = req.body;
+  try {
+    if (farmId) {
+      const farm = await Farm.findById(farmId).populate('crops');
+      if (!farm) {
+        return res.status(404).send('Farm not found');
+      }
+      return res.send(farm.crops);
+    }else{
+      res.send("Please provide a farm ID")
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while processing your request');
+  }
+});
 
-// get a crop by name
+// search for a crop by name
 router.get('/:crop', async (req,res)=> {
-  let cropName = req.params.crop;
-  cropName = cropName.toLowerCase();
-  const crop = await Crop.find({
-    Crop_Type: { $regex: new RegExp(cropName), $options: 'i' }
+  const { crop } = req.params;
+  const { farmId } = req.body;
+
+  // Check if farmId is provided
+  if (!farmId) {
+    return res.status(400).send('Farm ID is required');
+  }
+
+  // Find the farm by ID
+  const farm = await Farm.findById(farmId);
+  if (!farm) {
+    return res.status(404).send('Farm not found');
+  }
+
+  // Find the crop by name and farm
+  const crops = await Crop.find({
+    name: { $regex: new RegExp(crop), $options: 'i' },
+    farm: farmId
   });
-  res.send(crop)
-})
+
+  res.send(crops);
+});
 
 
 module.exports = router;
